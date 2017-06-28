0ec3ab4ec77bc8c341eb91cf1aacb1f1a7f55d90
Mike Merin <mikemerinweather@gmail.com>
2017-06-28 09:40:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 function findMinAndRemove(array){
 
+  let min = array[0]
+  let index = 0
+
+  array.forEach((x, i) => {
+    if (x < min) {
+      min = x
+      index = i
+    }
+  })
+
+  array.splice(index, 1)
+  return min
+
 }
 
 function insertionSort(array){
+
+  let sortedArray = []
+
+  while (array.length > 0) {
+    sortedArray.push(findMinAndRemove(array))
+  }
+
+  return sortedArray
 
 }
